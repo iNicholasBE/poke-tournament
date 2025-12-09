1ede5ba9a5356a05f6f5b44aeff0366efc9a3bcc
Nicholas D'hondt <nicholas@jobrunr.io>
2025-12-09 18:01:40 +0100

    new matches

--- schedule_data.php
+++ schedule_data.php
@@ -3,29 +3,34 @@
 
 // Round Robin Schedule (5 Weeks)
 $schedule = [
+    // Week 1: Blijft intact volgens uw wens
     1 => [
         ['Zeeman', 'Nxken'],
         ['AlvenBleken', 'Wasmachien1337'],
         ['MattiLeMattiBE', 'P4ulfiction'],
     ],
+    // Week 2: Nieuwe indeling zonder herhalingen
     2 => [
-        ['Zeeman', 'P4ulfiction'],
-        ['Nxken', 'MattiLeMattiBE'],
-        ['AlvenBleken', 'Wasmachien1337'],
+        ['Zeeman', 'AlvenBleken'],
+        ['Nxken', 'P4ulfiction'], // Ontbrekende match
+        ['Wasmachien1337', 'MattiLeMattiBE'], // Ontbrekende match (was herhaald in uw origineel)
     ],
+    // Week 3: Nieuwe indeling zonder herhalingen
     3 => [
         ['Zeeman', 'MattiLeMattiBE'],
-        ['P4ulfiction', 'AlvenBleken'],
-        ['Nxken', 'Wasmachien1337'],
+        ['P4ulfiction', 'Wasmachien1337'],
+        ['AlvenBleken', 'Nxken'],
     ],
+    // Week 4: Nieuwe indeling zonder herhalingen
     4 => [
-        ['Zeeman', 'Wasmachien1337'],
-        ['MattiLeMattiBE', 'P4ulfiction'],
-        ['Nxken', 'AlvenBleken'],
+        ['Zeeman', 'P4ulfiction'],
+        ['Wasmachien1337', 'Nxken'],
+        ['MattiLeMattiBE', 'AlvenBleken'],
     ],
+    // Week 5: Nieuwe indeling zonder herhalingen
     5 => [
-        ['Zeeman', 'AlvenBleken'],
-        ['Wasmachien1337', 'P4ulfiction'],
-        ['MattiLeMattiBE', 'Nxken'],
+        ['Zeeman', 'Wasmachien1337'],
+        ['AlvenBleken', 'P4ulfiction'],
+        ['Nxken', 'MattiLeMattiBE'],
     ],
 ];
