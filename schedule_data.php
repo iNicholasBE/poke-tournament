a84260140fb713d4b0c7a1c4a3267b6daceba309
Nicholas D'hondt <nicholas@jobrunr.io>
2025-12-09 18:07:00 +0100

    poke tournament

--- schedule_data.php
+++ schedule_data.php
@@ -3,31 +3,26 @@
 
 // Round Robin Schedule (5 Weeks)
 $schedule = [
-    // Week 1: Blijft intact volgens uw wens
     1 => [
         ['Zeeman', 'Nxken'],
         ['AlvenBleken', 'Wasmachien1337'],
         ['MattiLeMattiBE', 'P4ulfiction'],
     ],
-    // Week 2: Nieuwe indeling zonder herhalingen
     2 => [
         ['Zeeman', 'AlvenBleken'],
-        ['Nxken', 'P4ulfiction'], // Ontbrekende match
-        ['Wasmachien1337', 'MattiLeMattiBE'], // Ontbrekende match (was herhaald in uw origineel)
+        ['Nxken', 'P4ulfiction'], 
+        ['Wasmachien1337', 'MattiLeMattiBE'],
     ],
-    // Week 3: Nieuwe indeling zonder herhalingen
     3 => [
         ['Zeeman', 'MattiLeMattiBE'],
         ['P4ulfiction', 'Wasmachien1337'],
         ['AlvenBleken', 'Nxken'],
     ],
-    // Week 4: Nieuwe indeling zonder herhalingen
     4 => [
         ['Zeeman', 'P4ulfiction'],
         ['Wasmachien1337', 'Nxken'],
         ['MattiLeMattiBE', 'AlvenBleken'],
     ],
-    // Week 5: Nieuwe indeling zonder herhalingen
     5 => [
         ['Zeeman', 'Wasmachien1337'],
         ['AlvenBleken', 'P4ulfiction'],
